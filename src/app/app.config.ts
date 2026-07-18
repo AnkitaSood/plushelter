7b2ccfb7b39dda6b22b0576b93458d2ea1b53725
ankita <ankita.s33@gmail.com>
2026-07-18 18:33:10 -0500

    chore: add angular ssr

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,12 +3,13 @@ import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(), provideClientHydration()
   ]
 };
